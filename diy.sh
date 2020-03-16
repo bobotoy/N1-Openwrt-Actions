096fa6e48fae29bbdacd068690a80b51b4bf0c92
bobotoy <32072974+bobotoy@users.noreply.github.com>
2020-03-17 02:14:44 +0800

    Add files via upload

---
+++ diy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=================================================
+# Modify default IP
+sed -i "s/timezone='UTC'/timezone='CST-8'/" package/base-files/files/bin/config_generate
+sed -i "/timezone='CST-8'/a \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ set system.@system[-1].zonename='Asia/Shanghai'" package/base-files/files/bin/config_generate
