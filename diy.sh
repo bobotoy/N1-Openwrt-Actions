dd973702e6ee1b58fa62c286369f2126f9e7f059
bobotoy <32072974+bobotoy@users.noreply.github.com>
2020-03-17 14:32:19 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,6 +6,8 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-sed -i "s/timezone='UTC'/timezone='CST-8'/" package/base-files/files/bin/config_generate
-sed -i "/timezone='CST-8'/a \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ set system.@system[-1].zonename='Asia/Shanghai'" package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+# Timezone
+sed -i "s/timezone='UTC'/timezone='CST-8'/" package/base-files/files/bin/config_generate
+sed -i "/timezone='CST-8'/a set system.@system[-1].zonename='Asia/Shanghai'" ./package/base-files/files/bin/config_generate
+# firewall custom
