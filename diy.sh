01b257bed4c51d96115726b638128f1f00137427
bobotoy <32072974+bobotoy@users.noreply.github.com>
2020-03-17 14:55:57 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -11,3 +11,4 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 sed -i "s/timezone='UTC'/timezone='CST-8'/" package/base-files/files/bin/config_generate
 sed -i "/timezone='CST-8'/a set system.@system[-1].zonename='Asia/Shanghai'" ./package/base-files/files/bin/config_generate
 # firewall custom
+echo "iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE" >> package/network/config/firewall/files/firewall.user
