b6653afccc991bfdfb5981d0c60562a067bec928
bobotoy <32072974+bobotoy@users.noreply.github.com>
2020-03-17 13:32:28 +0800

    Update build.sh

--- n1/build.sh
+++ n1/build.sh
@@ -25,9 +25,9 @@ rm -f DockerImg-OpenwrtArm64-${TAG}.gz && \
 docker build -t ${IMG_NAME}:${TAG} . && \
 rm -f  openwrt-armvirt-64-default-rootfs-patched.tar && \
 rm -rf "$TMPDIR" && \
-docker save ${IMG_NAME}:${TAG} | pigz -9 > $OUTDIR/docker-img-openwrt-aarch64-${TAG}.gz
-docker login -u ${{ secrets.DOCKER_HUB_NAME }} -p ${{ secrets.DOCKER_HUB_PWD }}
-#gzip -dc docker-img-openwrt-aarch64-${TAG}.gz | docker load
+docker save ${IMG_NAME}:${TAG} | pigz -9 > $OUTDIR/docker-img-openwrt-aarch64-${TAG}.gz && \
+docker login -u ${{ secrets.DOCKER_HUB_NAME }} -p ${{ secrets.DOCKER_HUB_PWD }} && \
+#gzip -dc docker-img-openwrt-aarch64-${TAG}.gz | docker load && \
 docker push ${IMG_NAME}:${TAG}
 
 
