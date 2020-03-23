5ab4fa24f9961920b5d9ddd41f759b195d480b93
bobotoy <32072974+bobotoy@users.noreply.github.com>
2020-03-23 22:38:33 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -12,5 +12,5 @@ sed -i "s/timezone='UTC'/timezone='CST-8'/" package/base-files/files/bin/config_
 sed -i "/timezone='CST-8'/a set system.@system[-1].zonename='Asia/Shanghai'" ./package/base-files/files/bin/config_generate
 # firewall custom
 echo "iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE" >> package/network/config/firewall/files/firewall.user
-# Fix kpr's Makefile for adapt aarch64
+# Fix kpr Makefile for adapt aarch64
 sed -i 's/,arm/,aarch64/' package/luci-app-koolproxyR/Makefile
